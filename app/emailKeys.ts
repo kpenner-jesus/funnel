314edfe9302b7069000a70f38e8ded737e0b49b4
kpenner-jesus <73081110+kpenner-jesus@users.noreply.github.com>
2026-03-14 16:21:06 -0500

    Create emailKeys.ts

---
+++ app/emailKeys.ts
@@ -0,0 +1,6 @@
+export const EMAIL_KEYS = {
+  SERVICE_ID:        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID        ?? "",
+  TEMPLATE_ID:       process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID       ?? "",
+  GUEST_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_GUEST_TEMPLATE_ID ?? "",
+  PUBLIC_KEY:        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY        ?? "",
+};
